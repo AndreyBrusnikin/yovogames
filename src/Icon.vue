36037be83790dcaa1c7a0fc5d96180867bc40d45
AndreyBrusnikin <andreybrusnikin@gmail.com>
2021-05-06 00:03:22 +0300

    yovo

---
+++ src/Icon.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    svg(:class="'icon icon-'+id")
+        use(:xlink:href="file+'#'+id", :href="file+'#'+id")
+</template>
+<script>
+    export default {
+        name: "Icon",
+        props: {
+            id: {required: true},
+            file: {default: process.env.BASE_URL+BASE_PATH+ "css/sprites.svg"}
+        },
+        mounted() {
+        }
+    }
+</script>
